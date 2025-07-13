f337eb7c02aeaff964f92066dd692647b311519b
Daniil <stifflingdagger@gmail.com>
2025-07-13 22:55:50 +0300

    task1-2 fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,6 +1,6 @@
 function getShippingMessage(country, price, deliveryFee) {
 
-    totalPrice = price + deliveryFee;
+    let totalPrice = price + deliveryFee;
 
     return `Shipping to ${country} will cost ${totalPrice} credits`
 }
